959dc323100e75f8a2576eb5731ab07cb4521a49
Jordan Young <jordanyoung@Jordans-MacBook-Pro.local>
2025-01-24 11:47:24 -0600

    custom dataset class

---
+++ app/data.py
@@ -0,0 +1,60 @@
+from torch.utils.data import Dataset
+from typing import Tuple
+import json
+from app.constants import LABEL_ENCODINGS
+from transformers import BertTokenizer
+
+
+class SentencePairDataset(Dataset):
+    def __init__(self, sentence_pairs: list[tuple], labels: list[str], tokenizer: BertTokenizer):
+        """Initialize SentencePairDataset."""
+
+        self.sentence_pairs = sentence_pairs
+        self.labels = self.encode_labels(labels)
+        self.tokenizer = tokenizer
+
+    def __getitem__(self, idx):
+        """Get a sentence pair and label from the dataset."""
+        left, right = self.sentence_pairs[idx]
+        
+        left = self.tokenizer.encode_plus(left, max_length=512,padding='max_length',truncation=True, return_tensors="pt")
+        right = self.tokenizer.encode_plus(right, max_length=512,padding='max_length',truncation=True, return_tensors="pt")
+        label = self.labels[idx]
+
+        return left, right, label
+
+    def __len__(self):
+        """Get the length of the dataset."""
+        return len(self.labels)
+
+    def encode_labels(self, labels: list) -> list[list]:
+        """Encode dataset labels."""
+
+
+        encoded_labels = []
+        for label in labels:
+            encoded_labels.append(LABEL_ENCODINGS[label])
+        return encoded_labels
+
+
+
+def load_data(filename) -> Tuple[list[tuple],list[str]]:
+    """Loads sentence pair data from jsonl file."""
+
+    with open(filename, 'r') as jfile:
+        json_list = list(jfile)
+    
+    pairs = []
+    labels = []
+    for json_str in json_list:
+        res = json.loads(json_str)
+        sentence_1 = res['sentence1']
+        sentence_2 = res['sentence2']
+        label = res['gold_label']
+
+        if label in LABEL_ENCODINGS:
+            pairs.append((sentence_1,sentence_2))
+            labels.append(label)
+
+
+    return pairs, labels
